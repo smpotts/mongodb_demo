2cb2b6fbe4999d833032ade2bce79e3e4089a912
Shelby <potts.shelby@gmail.com>
2021-07-16 08:29:47 -0600

    added inserts and updates

--- src/main/java/com/spotts/mongodb_demo/DatabaseService.java
+++ src/main/java/com/spotts/mongodb_demo/DatabaseService.java
@@ -1,6 +1,5 @@
 package com.spotts.mongodb_demo;
 
-import com.mongodb.BasicDBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
@@ -14,33 +13,52 @@ import javax.annotation.PostConstruct;
 @Component
 public class DatabaseService {
 
+    private static final String DB_NAME = "java_test";
+    private static final String HOSTNAME = "localhost";
+    private static final int PORT = 27017;
     private static final Logger log = LoggerFactory.getLogger(DatabaseService.class);
+
+    private static MongoDatabase mongoDatabase;
 
     @PostConstruct
     public void setupMongo() {
-        MongoClient mongoClient = new MongoClient("localhost", 27017);
+        MongoClient mongoClient = new MongoClient(HOSTNAME, PORT);
+        mongoDatabase = mongoClient.getDatabase(DB_NAME);
         // print the names of the default databases
         for (String dbName : mongoClient.listDatabaseNames()) {
             log.info("Mongo DB has database: " + dbName);
         }
-
-        MongoDatabase database = mongoClient.getDatabase("java_test");
-//        database.createCollection("first_collection");
-
-        for (String collectionName : database.listCollectionNames()) {
+        for (String collectionName : mongoDatabase.listCollectionNames()) {
             log.info("Mongo DB has collection: " + collectionName);
         }
     }
 
-    public void saveInsert(MongoDatabase database, String collectionName) {
-        MongoCollection<Document> collection =  database.getCollection(collectionName);
+    public void saveInsert(String collectionName, String make, String model) {
+        MongoCollection<Document> collection =  mongoDatabase.getCollection(collectionName);
         Document document = new Document();
-        document.put("make", "Fender");
-        document.put("model", "Stratocaster");
+        document.put("make", make);
+        document.put("model", model);
         collection.insertOne(document);
+        log.info("Inserted document with make " + make + " and model " + model + ".");
     }
 
-    public void createCollection(MongoDatabase database, String collectionName) {
-        database.createCollection(collectionName);
+    public void saveUpdate(String collectionName, String model, String newModel) {
+        MongoCollection<Document> collection =  mongoDatabase.getCollection(collectionName);
+        Document query = new Document();
+        query.put("model", model);
+
+        Document newDocument = new Document();
+        newDocument.put("model", newModel);
+
+        Document updateObject = new Document();
+        updateObject.put("$set", newDocument);
+
+        collection.updateOne(query, updateObject);
+        log.info("Updated model from " + model + " to " + newModel + ".");
+    }
+
+    public void createCollection(String collectionName) {
+        mongoDatabase.createCollection(collectionName);
+        log.info("Created collection " + collectionName);
     }
 }
--- src/main/java/com/spotts/mongodb_demo/MongodbDemoApplication.java
+++ src/main/java/com/spotts/mongodb_demo/MongodbDemoApplication.java
@@ -1,7 +1,5 @@
 package com.spotts.mongodb_demo;
 
-import com.mongodb.MongoClient;
-import com.mongodb.client.MongoDatabase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,7 +8,12 @@ public class MongodbDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MongodbDemoApplication.class, args);
+
 		DatabaseService databaseService = new DatabaseService();
+//		databaseService.createCollection("car_collection");
+//		databaseService.saveInsert("car_collection", "Toyota", "RAV4");
+//		databaseService.saveInsert("car_collection", "Ford", "Mustang");
+		databaseService.saveUpdate("car_collection", "Mustang", "Bronco");
 	}
 
 }
